4a4bf98e00dc90ced35e6be41b29a941b2ca8b77
Jho-coder <69260356+Jho-coder@users.noreply.github.com>
2020-09-19 02:05:20 -0400

    Add files via upload

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f(): any {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.valid) {
+      return;
+    }
+  }
+
+  redirect(): void{
+    this.route.navigate(['./component1']);
+  }
+
+}
